be3d6d07b41767e3bdcb78a4a43a02d904c35a42
Angel Preciado <angel.morfeo@gmail.com>
2015-07-22 21:56:56 +0200

    crear preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,5 +1,6 @@
 var models = require('../models/models.js')
 
+//Autoload - factoriza el código si la ruta incluye :quizId
 exports.load = function(req, res, next, quizId){
 	models.Quiz.find(quizId).then(
 		function(quiz){
@@ -34,11 +35,28 @@ exports.show = function(req, res){
 };
 
 //GET /quizes/:id/answer
-
 exports.answer = function(req, res){
 	var resultado = "Incorrecto";
 	if(req.query.respuesta === req.quiz.respuesta){
 		resultado = "Correcto";
 	} 
 	res.render('quizes/answer',{quiz: req.quiz, respuesta: resultado});
+};
+
+//GET /quizes/new
+exports.new = function(req, res){
+	var quiz = models.Quiz.build( //crea objecto Quiz: esto nos lo facilita sequelize
+		{pregunta: "Pregunta", respuesta: "Respuesta"}
+	);
+	res.render('quizes/new',{quiz: quiz});
+};
+
+//POST /quizes/create
+exports.create = function(req, res){
+	var quiz = models.Quiz.build(req.body.quiz);
+
+	//guarda en DB los campos pregunta y respuesta de quiz
+	quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+		res.redirect('/quizes');	
+	})
 };
