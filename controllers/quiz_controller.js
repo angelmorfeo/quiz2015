e0c3b56f734e9919b8ae713a5bbfecd739a31d7d
Angel Preciado <angel.morfeo@gmail.com>
2015-07-23 19:46:11 +0200

    Preguntas con tematica

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -46,7 +46,7 @@ exports.answer = function(req, res){
 //GET /quizes/new
 exports.new = function(req, res){
 	var quiz = models.Quiz.build( //crea objecto Quiz: esto nos lo facilita sequelize
-		{pregunta: "Pregunta", respuesta: "Respuesta"}
+		{pregunta: "Pregunta", respuesta: "Respuesta", tema: "otro"}
 	);
 	res.render('quizes/new',{quiz: quiz, errors: []});
 };
@@ -61,7 +61,7 @@ exports.create = function(req, res){
 				res.render('quizes/new', {quiz: quiz, errors: err.errors});
 			} else {
 				//guarda en DB los campos pregunta y respuesta de quiz
-				quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+				quiz.save({fields: ["pregunta", "respuesta", "tema"]}).then(function(){
 					res.redirect('/quizes');	
 				})
 			}
@@ -79,6 +79,7 @@ exports.edit = function(req, res){
 exports.update = function(req, res){
 	req.quiz.pregunta = req.body.quiz.pregunta;
 	req.quiz.respuesta = req.body.quiz.respuesta;
+	req.quiz.tema = req.body.quiz.tema;
 
 	req.quiz.validate()
 	.then(
@@ -87,7 +88,7 @@ exports.update = function(req, res){
 				res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
 			} else {
 				//guarda en DB los campos pregunta y respuesta de quiz
-				req.quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+				req.quiz.save({fields: ["pregunta", "respuesta", "tema"]}).then(function(){
 					res.redirect('/quizes');	
 				})
 			}
--- models/quiz.js
+++ models/quiz.js
@@ -1,13 +1,17 @@
 module.exports = function (sequelize, DataTypes) {
 	return sequelize.define(
 		'Quiz',
-		{   pregunta: {
+		{   
+			pregunta: {
 				type: DataTypes.STRING, 
 				validate: {notEmpty: {msg: "-> Falta pregunta" }}
-				},
+			},
 			respuesta: {
 				type: DataTypes.STRING, 
 				validate: {notEmpty: {msg: "-> Falta respuesta" }}
+			},
+			tema: {
+				type: DataTypes.STRING
 			}
 		}
 	);
