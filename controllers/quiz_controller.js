d4cdf17182ac1c514e070a15496854ec4ad386f5
Angel Preciado <angel.morfeo@gmail.com>
2015-07-23 18:52:30 +0200

    Editar preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -54,16 +54,40 @@ exports.new = function(req, res){
 //POST /quizes/create
 exports.create = function(req, res){
 	var quiz = models.Quiz.build(req.body.quiz);
-	console.log('Pregunta: ' + quiz.pregunta);
-	console.log('Respuesta: ' + quiz.respuesta);
-	console.log('Validate: ' + quiz.validate());
-	quiz.validate().then(
+	quiz.validate()
+	.then(
 		function(err){
 			if(err){
 				res.render('quizes/new', {quiz: quiz, errors: err.errors});
 			} else {
 				//guarda en DB los campos pregunta y respuesta de quiz
 				quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
+					res.redirect('/quizes');	
+				})
+			}
+		}
+	);
+};
+
+//GET /quizes/:id/edit
+exports.edit = function(req, res){
+	var quiz = req.quiz; //autoload de instancia de quiz
+	res.render('quizes/edit',{quiz: quiz, errors: []});
+};
+
+//PUT /quizes/:id
+exports.update = function(req, res){
+	req.quiz.pregunta = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+
+	req.quiz.validate()
+	.then(
+		function(err){
+			if(err){
+				res.render('quizes/edit', {quiz: req.quiz, errors: err.errors});
+			} else {
+				//guarda en DB los campos pregunta y respuesta de quiz
+				req.quiz.save({fields: ["pregunta", "respuesta"]}).then(function(){
 					res.redirect('/quizes');	
 				})
 			}
