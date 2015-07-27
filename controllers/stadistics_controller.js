bd2f59d1b47765d157515524b88d4cde76a5e42c
Angel Preciado <angel.morfeo@gmail.com>
2015-07-27 19:00:21 +0200

    estadisticas v7 logs

--- controllers/stadistics_controller.js
+++ controllers/stadistics_controller.js
@@ -6,26 +6,31 @@ exports.process = function(req, res){
 	var quizNum, commentNum, avgCommnent, quizComment, quizNoComment;
 	models.Quiz.count().then(function(quiz) {
 		quizNum = quiz;
+		console.log('1');
 	models.Comment.count().then(function(comment) {		
 		commentNum = comment;
+		console.log('2');
 //	models.Comment.aggregate('id', 'avg', {group: ["QuizId"]}).then(function(avg) {		
 		//avgComment = avg;
 		if (quizNum == 0)
 			avgComment = 0;
 		else
 			avgComment = commentNum / quizNum;
+		console.log('3');
 	models.Quiz.count({
 				include: [{model: models.Comment}],
 				where: ["QuizId not null"],
 				distinct: ["id"]
 			}).then(function(count) {
 		quizComment = count;
+		console.log('4');
 	models.Quiz.count({
                                 include: [{model: models.Comment}],
                                 where: ["QuizId is null"],
                                 distinct: ["id"]
                         }).then(function(count) {
 		quizNoComment = count;
+		console.log('5');
 
 		res.render('quizes/stadistics', {
 			quiz: quizNum,
